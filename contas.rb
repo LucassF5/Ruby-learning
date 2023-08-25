797afd138f70258650eec724b1e6159805184ebf
L F <franco100lucas@gmail.com>
2023-08-24 21:25:03 -0300

    feat(file): adding a code with expressions

---
+++ contas.rb
@@ -0,0 +1,8 @@
+print "Digite um número: "
+num1 = gets.chomp.to_f
+#    .to_i transforma a string em inteiro
+
+print 'Digite outro número: '
+num2 = gets.chomp.to_i
+
+puts "A soma dos dois valores é #{num1} + #{num2} = #{num1 + num2}"
