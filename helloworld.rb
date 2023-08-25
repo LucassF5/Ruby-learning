1e08e3d4bcef2eb4bc5235e952099974af28eda1
L F <franco100lucas@gmail.com>
2023-08-24 21:25:33 -0300

    feat(file): adding features

--- helloworld.rb
+++ helloworld.rb
@@ -7,7 +7,10 @@ name = gets.chomp #Usando o gets.chomp para pegar o input do usuário
 print "Digite seu sobrenome: "
 sobrenome = gets.chomp
 
-puts "Olá #{name} #{sobrenome}"
+print "Qual sua idade? "
+idade = gets.chomp
+
+puts "Olá #{name} #{sobrenome}, com #{idade} anos"
 # Colocando #{} dentro de uma string, você pode colocar uma variável dentro dela
 
 #Usando print é impresso na tela e o cursor fica na mesma linha
