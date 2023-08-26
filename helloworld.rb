2fbf283d5fbde4040eb3daaa3f9059050f6ab6fb
L F <franco100lucas@gmail.com>
2023-08-26 20:23:55 -0300

    Novos dados

--- helloworld.rb
+++ helloworld.rb
@@ -14,4 +14,11 @@ puts "Olá #{name} #{sobrenome}, com #{idade} anos"
 # Colocando #{} dentro de uma string, você pode colocar uma variável dentro dela
 
 #Usando print é impresso na tela e o cursor fica na mesma linha
-#Usando puts é impresso na tela e o cursor vai para a próxima linha
+#Usando puts é impresso na tela e o cursor vai para a próxima linha 
+
+puts "#{sleep 2} #{name} Bom dia!!"
+# Usando #{sleep numero_de_segundos} vai dar uma pausa na execução do código na quantidade de segundos estabelecida
+
+for num 1..5
+    puts num
+end
