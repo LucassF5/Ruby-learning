9c954083363be72d6a3192900dd6d4bb8c1bef1d
L F <franco100lucas@gmail.com>
2024-06-19 03:09:51 -0300

    feat: Add category search method

--- loja_virtual/lib/biblioteca.rb
+++ loja_virtual/lib/biblioteca.rb
@@ -12,4 +12,25 @@ class Biblioteca
     @livros.values.flatten
   end
 
+  def livros_por_categoria(categoria)
+    @livros[categoria].each do |livros|
+      # para cada valor de categoria da instâmcia de biblioteca inicia um bloco
+      yield livros if block_given?
+      # Se o bloco foi passado, executa o bloco, caso contrário, não faz nada
+    end
+  end
+
 end
+
+require File.expand_path("livro", File.dirname(__FILE__))
+livro = Livro.new("Lucas Franco", 123, 250, 50, :testes)
+livro2 = Livro.new("Maria", 234, 300, 79.9, :acao)
+livro3 = Livro.new("Lua", 123, 300, 64.9, :testes)
+
+
+biblioteca = Biblioteca.new
+biblioteca.adiciona livro
+biblioteca.adiciona livro2
+biblioteca.adiciona livro3
+
+biblioteca.livros_por_categoria :acao
