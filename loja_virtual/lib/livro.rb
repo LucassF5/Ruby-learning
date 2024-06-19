8251b07177582fa56fc32d30091a809c702abf7e
L F <franco100lucas@gmail.com>
2024-06-19 03:08:08 -0300

    feat: Adding titulo parameter to class

--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -1,17 +1,20 @@
 class Livro
-  attr_accessor :preco # Implementa getters e setters
-  attr_reader :categoria, :isbn
+  attr_accessor :valor # Implementa getters e setters
+  attr_reader :categoria, :autor, :titulo
 
-  def initialize(autor, isbn="1", numero_de_paginas, preco, categoria)
+  def initialize(titulo, autor, isbn="1", numero_de_paginas, valor, categoria)
+    @titulo = titulo
     @autor = autor
     @isbn = isbn
     @numero_de_paginas = numero_de_paginas
-    @preco = preco
+    @valor = valor
     @categoria = categoria
   end
 
   def to_s
-    "Autor: #{@autor}, ISBN: #{@isbn}, Número de páginas: #{@numero_de_paginas}, Preço: #{@preco}, Categoria: #{@categoria}"
+      "Autor: #{@autor}, ISBN: #{@isbn},
+      Número de páginas: #{@numero_de_paginas},
+      Valor: #{@valor}, Categoria: #{@categoria}"
   end
 
   def eql?(outro_livro) # Sobrescrevendo o método eql? para comparar objetos
@@ -25,21 +28,21 @@ end
 
 
 # Criando objetos da classe Livro
-livro = Livro.new("Lucas Franco", 123, 250, 50, :testes)
-livro2 = Livro.new("Maria", 234, 300, 79.9, :acao)
-livro3 = Livro.new("Lua", 123, 300, 64.9, :comedia)
+# livro = Livro.new("Lucas Franco", 123, 250, 50, :testes)
+# livro2 = Livro.new("Maria", 234, 300, 79.9, :acao)
+# livro3 = Livro.new("Lua", 123, 300, 64.9, :comedia)
 
 # Comparando objetos com o método eql?
-puts livro.eql?(livro2) # false
-puts livro2.eql?(livro3) # false
-puts livro.eql?(livro3) # true => ISBNs iguais
+# puts livro.eql?(livro2) # false
+# puts livro2.eql?(livro3) # false
+# puts livro.eql?(livro3) # true => ISBNs iguais
 
 # Comparando objetos com o método hash
-puts livro.hash == livro2.hash # false
-puts livro2.hash == livro3.hash # false
-puts livro.hash == livro3.hash # true => ISBNs iguais
+# puts livro.hash == livro2.hash # false
+# puts livro2.hash == livro3.hash # false
+# puts livro.hash == livro3.hash # true => ISBNs iguais
 
-require 'set'
+# require 'set'
 =begin
   Set é uma coleção de valores não ordenada, sem elementos duplicados.
   Para evitar duplicidade o Set utiliza os métodos eql? e hash.
@@ -47,12 +50,12 @@ require 'set'
 =end
 
 # Criando um conjunto de livros
-livros = Set.new [livro, livro2, livro3]
+# livros = Set.new [livro, livro2, livro3]
 
 # Iterando sobre o conjunto de livros
-livros.each do |livro|
-  puts livro
-end
+# livros.each do |livro|
+  # puts livro
+# end
 
 # Saída:
 # Autor: Lucas Franco, ISBN: 123, Número de páginas: 250, Preço: 50, Categoria: testes
