90fdca71bcc61c7b35dcc184605cf57fd31f07e1
L F <franco100lucas@gmail.com>
2024-06-22 16:21:17 -0300

    feat: Creating CD and FormatadorMoeda classes

---
+++ loja_virtual/lib/cd.rb
@@ -0,0 +1,22 @@
+require File.expand_path("loja_virtual/lib/midia")
+require File.expand_path("loja_virtual/lib/formatador_moeda")
+
+class CD < Midia
+
+  def initialize(titulo, valor, categoria)
+    super()
+    @titulo = titulo
+    @valor = valor
+    @categoria = categoria
+  end
+
+  def to_s
+    %Q{ Titulo: #{@titulo}, Valor: #{@valor}, Categoria: #{@categoria} }
+  end
+end
+
+windows = CD.new("Windows 95", 239.9, :sistemas_operacionais)
+p windows
+puts windows
+windows.extend FormatadorMoeda # Extendeu o módulo para poder ser usado apenas nessa instância
+puts windows.valor_formatado
