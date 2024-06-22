846f165f7729c5950055ef537369867250ae34db
L F <franco100lucas@gmail.com>
2024-06-22 17:15:48 -0300

    feat: Add desconto and valor_com_desconto methods to Midia class

--- loja_virtual/lib/midia.rb
+++ loja_virtual/lib/midia.rb
@@ -1,4 +1,22 @@
 class Midia
   attr_reader :valor
   attr_accessor :titulo
+
+  def initialize
+    @desconto = 0.1
+  end
+
+  def valor_com_desconto
+    @valor - desconto
+  end
+
+  private
+
+  def desconto
+    @valor * @desconto
+  end
+
+  def valor_formatado
+    "R$ #{@valor}"
+  end
 end
